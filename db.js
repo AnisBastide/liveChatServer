529a03ba5e72bc03c9a72c1900b23f2f99809a61
anis bastide <anis.bastide@gmail.com>
2021-11-24 16:45:57 +0100

    cleaned the db file and the main file. Some refacto

--- db.js
+++ db.js
@@ -2,17 +2,59 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 class Database{
-    constructor() {
-        this.connect()
-    }
-    async connect() {
-        await mongoose.connect(process.env.URL_DB);
-    }
     dbSchema = new mongoose.Schema({
         mail: String,
         password: String,
         admin: { type: Boolean, default: false }
     });
     Auth = mongoose.model('auth', this.dbSchema);
+    constructor() {
+        this.connect()
+    }
+    async connect() {
+        await mongoose.connect(process.env.URL_DB);
+    }
+
+    async addUser(email, password,isAdmin) {
+        if (this.validateEmail(email) && this.validatePassword(password)) {
+            let auth = new this.Auth({
+                mail: email,
+                password: password,
+                admin: isAdmin
+            })
+            await auth.save();
+            return auth
+        }
+    }
+
+    async deleteUser(userId) {
+        await this.Auth.deleteUser(userId);
+        let deletedUser = this.getUser(userId)
+        if(deletedUser){
+            return false
+        }
+        return true
+    }
+    async getUser(userId) {
+        return this.Auth.findById(userId)
+        }
+
+    async updateUser(user,newUserInfo) {
+        this.Auth.updateOne(user,newUserInfo);
+    }
+
+    validateEmail(mail) {
+        var re = /\S+@\S+.\S+/;
+        return re.test(mail);
+    }
+    validatePassword(password) {
+        var re =/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z0-9]).{8,}$/
+        return re.test(password);
+    }
+
+    async isAdmin(userId){
+        let user = await this.getUser(userId);
+        return user.admin;
+    }
 }
 module.exports = Database
--- main.js
+++ main.js
@@ -11,75 +11,46 @@ nunjucks.configure('views', {
     express: app
 })
 
-// app.use(express.static('public'))
+app.use(express.static('public'))
 
 app.use('/pages', router)
 
 let database = new db()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-// app.get('/', async (req, res) => {
-//     res.send('Hello World!')
-//     let auth = new database.Auth({mail:"pierre@gmail.com",password:"password"})
-//     await auth.save();
-//     let  find = await database.Auth.find({mail:"pierre@gmail.com"});
-//     res.json(find);
 
-// })
+
 app.post('/register', async (req, res) => {
-    console.log(req.body);
-    let find = await database.Auth.findOne({mail:req.body['mail']})
-    console.log(find);
-    if (!find) {
-        if(validateEmail(req.body['mail']) && validatePassword(req.body['password'])){
-            let auth = new database.Auth({mail:req.body['mail'],password:req.body['password'], admin:req.body['admin']})
-            await auth.save();
-        }
+    let user = await database.Auth.findOne({mail:req.body['mail']})
+    if (!user) {
+        await database.addUser(req.body['mail'],req.body['password'])
     }
-    
-    res.json(find)
-    res.end()
-})
-app.delete('/delete', async(req, res) => {
-    console.log(req.body)
-    let find = await database.Auth.findOne({mail:req.body['mail']})
-    console.log(find)
-    find.delete();
-    res.json(find)
-    res.end()
-})
-app.patch('/:id', async(req, res) => {
-    if(isAdmin(req.params.id)){
-        res.render('home.html', {
-            title : 'Vous êtes admin'
-        })
-    }
-    console.log(req.params.id)
-    let find = await database.Auth.findOne({mail:'pierreTest@maiffffffl.frhgvkjgv'})
-    var findBis = find;
-    findBis.mail = 'update@test.fr';
-    console.log(find)
-    find.update(find, findBis);
-    res.json(findBis)
     res.end()
 })
 
-app.get('/', async (req, res) => {
-    
+app.delete('/:id', async(req, res) => {
+    let deleted = await database.deleteUser(req.params.id)
+    if(deleted){
+        res.end()
+    }
+    else{
+        res.json('User could not be deleted')
+    }
+})
+
+app.patch('/:id', async(req, res) => {
+    let user = await database.getUser('id')
+    let newUserInfo = new  database.Auth({mail:req.body['mail'],password:req.body['password']})
+    await database.updateUser(user,newUserInfo)
+    res.end()
+})
+
+app.get('/:id', async (req, res) => {
+    return await database.getUser(req.params.id)
 })
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
-function validateEmail(mail) {
-    var re = /\S+@\S+.\S+/;
-    return re.test(mail);
-}
-function validatePassword(password) {
-    var re =/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z0-9]).{8,}$/
-    return re.test(password);
-}
-async function isAdmin(userId){
-    let user = await database.Auth.findById(userId);
-    return user.isAdmin;
-}
+
+
